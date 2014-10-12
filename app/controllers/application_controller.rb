fdca06a717e28b9359c9316eff3a36f2f36635d9
Laurentiu Ion <ionlaurentiucristian@gmail.com>
2014-10-12 13:45:33 -0700

    temp changes: remove cookie,ip,mobile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,14 +14,14 @@ class ApplicationController < ActionController::Base
     protected
 
     def ref_to_cookie
-        if params[:ref] && !Rails.application.config.ended
-            if !User.find_by_referral_code(params[:ref]).nil?
-                cookies[:h_ref] = { :value => params[:ref], :expires => 1.week.from_now }
-            end
-
-            if request.env["HTTP_USER_AGENT"] and !request.env["HTTP_USER_AGENT"].include?("facebookexternalhit/1.1")
-                redirect_to proc { url_for(params.except(:ref)) }  
-            end
-        end
+        # if params[:ref] && !Rails.application.config.ended
+        #     if !User.find_by_referral_code(params[:ref]).nil?
+        #         cookies[:h_ref] = { :value => params[:ref], :expires => 1.week.from_now }
+        #     end
+        #
+        #     if request.env["HTTP_USER_AGENT"] and !request.env["HTTP_USER_AGENT"].include?("facebookexternalhit/1.1")
+        #         redirect_to proc { url_for(params.except(:ref)) }
+        #     end
+        # end
     end
 end
