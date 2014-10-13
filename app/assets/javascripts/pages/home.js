ba8de04ccd2b36f37b7848e7ca8d763219324768
Laurentiu Ion <ionlaurentiucristian@gmail.com>
2014-10-12 21:32:56 -0700

    forgot to add some css

---
+++ app/assets/javascripts/pages/home.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/* global app */
+var Backbone = require('../shims/backbone');
+var View = Backbone.View;
+var templates = require('../lib/templates');
+
+module.exports = View.extend({
+  pageTitle: 'Wabbit Messenger',
+  template: templates.pages.home,
+  events: {
+    'click .register': 'register'
+  },
+  render: function () {
+    this.$el.html(this.template());
+    return this;
+  },
+
+  customDocumentClasses: function () {
+    return ['home-page'];
+  },
+});
